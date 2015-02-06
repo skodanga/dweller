f3c8df165a8e85432408220d0fe0e7aed50ace15
Shyam Kodangattil <shyam@Shyams-Air.home>
2015-02-05 21:09:56 -0500

    initial scaffold

---
+++ id_assigner.py
@@ -0,0 +1,14 @@
+'''
+Rule for well ID should be customizable				
+Target_Libray_Round_Branch_Subbranch_Plate_Well				
+				
+Target 	TNF			
+Library	Lib1			
+Round	3			
+Branch	A			
+Subbranch	1	subbranch should be optional		
+Plate	1 to N			
+Well	A1 to H12			
+				
+Plate format should customizable				
+'''
